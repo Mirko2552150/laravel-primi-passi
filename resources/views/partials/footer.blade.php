643e18703ee7cc49140c9623ad10279e7f45ad8a
Mirko <mirko.longo.1987@gmail.com>
2020-05-06 15:30:01 +0200

    Inserimento Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,41 @@
-<footer></footer>
+<footer>
+  <div class="box-col">
+    <div class="col">
+      <img class="logo" src="{{'img/marchio-sito-test.png'}}" alt="La Molisana">
+      <h5><a href="#">xxxxxxx</a></h5>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+    </div>
+    <div class="col">
+      <h5><a href="#">xxxxxxx</a></h5>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <h5><a href="#">xxxxxxx</a></h5>
+      <p><a href="#">xxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+    </div>
+    <div class="col">
+      <h5><a href="#">xxxxxxx</a></h5>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+      <p><a href="#">xxxxxxxxxxxxxxxx</a></p>
+    </div>
+  </div>
+  <div class="box-bg"></div>
+</footer>
